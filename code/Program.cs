d0321e87d3f9fafe0a0bc90e6dfb18424c1f2ff6
Zukhra <z.malikova114@gmail.com>
2022-09-10 22:15:49 +0600

    Программный код

---
+++ code/Program.cs
@@ -0,0 +1,40 @@
+﻿string [] CreateRandomArray(int size)
+{
+    string [] newArray = new string [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input element:");
+        Console.Write($"Input {i+1} element of {size}: ");
+        newArray [i] = Console.ReadLine();
+    } return newArray;
+}
+
+void ShowArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write(" " + array [i] + " ");
+    Console.WriteLine();
+}
+
+void MethodArray (string [] array)
+{
+     string[] newarr = new string[array.Length];
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3)
+        {
+           newarr[i]=array[i];
+           Console.Write(" " + newarr[i] + " "); 
+        }
+        else Console.Write("there is no such element");
+     }
+        Console.WriteLine(); 
+}
+
+Console.Write("Input size our array: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string [] Array = CreateRandomArray(x);
+Console.Write("Our array: ");
+ShowArray(Array); 
+Console.Write("Array with elements of 3 symbol: ");    
+MethodArray(Array);
